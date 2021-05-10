2282f788d567a6fb3c1ad0bed1d97c257a41c6a9
Anthonios Partheniou <partheniou@google.com>
2021-05-09 20:56:14 -0400

    chore: copy files from googleapis-gen ee56c3493ec6aeb237ff515ecea949710944a20f

---
+++ owl-bot-staging/v1/google/cloud/accessapproval_v1/__init__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from .services.access_approval import AccessApprovalClient
+from .types.accessapproval import AccessApprovalSettings
+from .types.accessapproval import AccessLocations
+from .types.accessapproval import AccessReason
+from .types.accessapproval import ApprovalRequest
+from .types.accessapproval import ApproveApprovalRequestMessage
+from .types.accessapproval import ApproveDecision
+from .types.accessapproval import DeleteAccessApprovalSettingsMessage
+from .types.accessapproval import DismissApprovalRequestMessage
+from .types.accessapproval import DismissDecision
+from .types.accessapproval import EnrolledService
+from .types.accessapproval import EnrollmentLevel
+from .types.accessapproval import GetAccessApprovalSettingsMessage
+from .types.accessapproval import GetApprovalRequestMessage
+from .types.accessapproval import ListApprovalRequestsMessage
+from .types.accessapproval import ListApprovalRequestsResponse
+from .types.accessapproval import ResourceProperties
+from .types.accessapproval import UpdateAccessApprovalSettingsMessage
+
+
+__all__ = (
+    'AccessApprovalSettings',
+    'AccessLocations',
+    'AccessReason',
+    'ApprovalRequest',
+    'ApproveApprovalRequestMessage',
+    'ApproveDecision',
+    'DeleteAccessApprovalSettingsMessage',
+    'DismissApprovalRequestMessage',
+    'DismissDecision',
+    'EnrolledService',
+    'EnrollmentLevel',
+    'GetAccessApprovalSettingsMessage',
+    'GetApprovalRequestMessage',
+    'ListApprovalRequestsMessage',
+    'ListApprovalRequestsResponse',
+    'ResourceProperties',
+    'UpdateAccessApprovalSettingsMessage',
+'AccessApprovalClient',
+)
